5d99df2c1bcfb79cce9c5a06d87b96d0c7704755
dspodina <spodina.darya@gmail.com>
2024-04-26 09:01:28 +0200

    project developed

--- src/dom.js
+++ src/dom.js
@@ -1,5 +1,6 @@
 const dom = {
-    parent: document.getElementById('parent'),
+    btnContainer: document.querySelector('.btn-container'),
+    itemsContainer: document.querySelector('.items-container'),
 };
 
 export default dom;
